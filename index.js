b81e362df8fa1f31c8a895ce3f13a9f281a6eed4
bikram <bikrambasnet1@gmail.com>
2016-05-14 17:17:09 +0545

    added s3 bucket

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var S3Adapter = require('parse-server').S3Adapter;
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -19,7 +20,12 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  filesAdapter: new S3Adapter(
+    process.env.AWS_ACCESS_KEY_ID || "AKIAJTRVCIO6Q5KUNLSA",
+    process.env.AWS_SECRET_ACCESS_KEY || "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",
+    process.env.BUCKET_NAME || "runfitdevelopment"
+  )
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
