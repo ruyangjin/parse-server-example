15a063e7c7627772fa3294e714ba747f11f260e5
ruyangjin <ruyangjin@gmail.com>
2016-05-17 10:26:17 +0545

    changed s3 keys

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
   filesAdapter: new S3Adapter(
-    process.env.AWS_ACCESS_KEY_ID || "AKIAJTRVCIO6Q5KUNLSA",
-    process.env.AWS_SECRET_ACCESS_KEY || "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",
-    process.env.BUCKET_NAME || "runfitdevelopment"
+    process.env.AWS_ACCESS_KEY_ID || "AKIAJUSZVGOUU2ZRMNIQ",
+    process.env.AWS_SECRET_ACCESS_KEY || "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",
+    process.env.BUCKET_NAME || "runfitdevelopment",
+    {directAccess: true}
   )
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
